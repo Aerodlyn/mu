228e7fb76a95ca7c05c045648a21df76ad6af79f
Patrick Jahnig <psj516@vt.edu>
2021-03-25 22:24:20 -0400

    Adding community model

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.db.models import (
+    BooleanField,
+    CharField,
+    Model,
+    SlugField,
+    TextField
+)
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+# Community Model
+class Community (Model):
+    class Meta:
+        verbose_name_plural: str = "communities"
+
+    name        : CharField     = CharField (max_length = 64, primary_key = True)
+    description : TextField     = TextField ()
+    private     : BooleanField  = BooleanField (default = False)
+    slug        : SlugField     = SlugField (editable = False)
+
+    def __str__ (self) -> str:
+        return f"{ self.name }: /communities/{ self.slug }"
+
+    # Override
+    def save (self, *args: list, **kwargs: dict):
+        self.slug = slugify (self.name)
+        super ().save (*args, **kwargs)
+# End Community Model
