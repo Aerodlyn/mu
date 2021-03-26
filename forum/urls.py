8751fb69b8fb5efaf02c3357e19ea4fc392d11c8
Patrick Jahnig <psj516@vt.edu>
2021-03-26 16:35:47 -0400

    Beginning to work on communities view

--- forum/urls.py
+++ forum/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import (
+    CommunityListView,
+    IndexView
+)
 
 urlpatterns = [
-    path ("", IndexView.as_view (), name = "index")
+    path ("", IndexView.as_view (), name = "index"),
+    path ("communities", CommunityListView.as_view (), name = "community-list")
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+
+from .models import Community
+
+class CommunityListView (ListView):
+    paginate_by     : int       = 10
+    ordering        : list      = [ "name" ]
+    template_name   : str       = "forum/community/community-list.html"
+
+    model           : Community = Community
 
 class IndexView (TemplateView):
     template_name: str = "forum/index.html"
